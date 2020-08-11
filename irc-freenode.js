64f6b7a11aec8e0ef9d31683728a19d6b11a5207
realrasengan <andysl@gmail.com>
2020-08-11 07:37:38 -0700

    updates

---
+++ irc-freenode.js
@@ -0,0 +1,79 @@
+// MIT Licensed etc.
+
+const request = require('request');
+const irc = require('irc');
+const {NodeClient} = require('hs-client');
+const {Network} = require('hsd');
+const network = Network.get('main');
+
+const clientOptions = {
+  network: network.type,
+  port: network.rpcPort,
+  apiKey: 'fb46901f7d93eba7b9e37c1164081b90c9e3951ea28478794d921ff80da6ae5b'
+}
+
+const nodeclient = new NodeClient(clientOptions);
+
+let client = new irc.Client('irc.freenode.net', 'handshakeb0t', {
+  userName: "hns",
+  realName: "handshake bot",
+  channels: ['#handshake'],
+  //secure: true,
+//  sasl: true,
+  //password: "can uncomment sasl and put password here"
+});
+client.addListener('raw',function(msg) {
+  console.dir(msg);
+});
+client.addListener('message#',async function(sender,target,text) {
+  if(target=="#handshake") {
+    let message=text.split(" ");
+    if(message[0]=="hns") {
+      switch(message[1].toUpperCase()) {
+        case "INFO":
+          if(message[2]=="" || message[2]==null) {
+            result = await nodeclient.execute('getinfo');
+            request('https://api.coingecko.com/api/v3/coins/handshake',{json:true},function(err,res,body) {
+              if(err)
+                return;
+              else
+                client.say(target,"Handshake is currently at block "+result.blocks+" with difficulty "+result.difficulty+".  Currently, a single HNS coin is worth $"+body.market_data.current_price.usd);
+            });
+          }
+          else {
+            result = await nodeclient.execute('getnameinfo', [ message[2] ]);
+            if(result.start.reserved) {
+              client.say(target,message[2]+" was pre-reserved");
+            }
+            else if(result.info==null) {
+              client.say(target,message[2]+" will be available at block "+result.start.start);
+            }
+            else {
+              switch(result.info.state) {
+                case "CLOSED":
+                  addressresult = await nodeclient.getCoin(result.info.owner.hash, result.info.owner.index);
+                  address=addressresult.address;
+                  client.say(target,message[2]+" was purchased for "+result.info.value/1000000
+                                              +" HNS by "+address+" and started at block " + result.info.stats.renewalPeriodStart + " and must be renewed before block "+result.info.stats.renewalPeriodEnd);
+                  break;
+                case "REVEAL":
+                  addressresult = await nodeclient.getCoin(result.info.owner.hash, result.info.owner.index);
+                  address=addressresult.address;
+                  client.say(target,message[2]+"'s auction started at block "+result.info.height+" and is in reveal since block "+result.info.stats.revealPeriodStart+" with current highest bidder "
+                                              +address+". The current 2nd highest bid is "+result.info.value/1000000+" HNS and the reveal will end at block "+result.info.stats.revealPeriodEnd);
+                  break;
+                case "BIDDING":
+                  client.say(target,message[2]+" was opened for auction at block "+result.info.height+" and has been open for bids since block "+result.info.stats.bidPeriodStart+" and the bidding will end at block "+result.info.stats.bidPeriodEnd);
+                  break;
+                case "OPENING":
+                  client.say(target,message[2]+" has been opening since block " + result.info.stats.openPeriodStart + " and bidding will start at block "+result.info.stats.openPeriodEnd);
+                  break;
+              }
+            }
+          }
+          break;
+      }
+    }
+  }
+});
+
---
+++ websocket-freenode.js
@@ -0,0 +1,83 @@
+const he = require('he');
+const punycode = require('punycode');
+
+const IRC_SERVER = "irc.freenode.net";
+const IRC_NICK = "handshakel1ve";
+const IRC_CHANNELS = ['#handshake-live'];
+
+const request = require('async-request');
+const fs = require('fs');
+const irc = require('irc');
+const net = require('net');
+
+const _b = String.fromCharCode(2);
+
+var express = require('express');
+var app = express();
+const port = 11112;
+
+app.listen(port, () => console.log('Listening... '+port));
+
+let client = new irc.Client(IRC_SERVER, IRC_NICK, {
+  userName: IRC_NICK,
+  realName: "Handshake Live",
+  channels: IRC_CHANNELS,
+});
+
+
+const {NodeClient} = require('hs-client');
+const {Network} = require('hsd');
+const network = Network.get('main');
+const {TX} = require('hsd');
+const {BloomFilter} = require('bfilter');
+
+const nodeOptions = {
+  network: network.type,
+  port: network.rpcPort,
+  apiKey: 'fb46901f7d93eba7b9e37c1164081b90c9e3951ea28478794d921ff80da6ae5b'
+}
+const nodeClient = new NodeClient(nodeOptions);
+
+(async () => {
+  const filter = BloomFilter.fromRate(1, 1, BloomFilter.flags.NONE);
+  filter.filter.fill(0xff);  // hack so that everything matches the filter
+
+  try {
+    await nodeClient.open();
+    await nodeClient.call('set filter', filter.encode());
+    await nodeClient.call('watch mempool');
+  }
+  catch {
+    process.exit();
+  }
+})();
+
+nodeClient.bind('tx', async (raw) => {
+  let _tx = TX.fromRaw(raw).hash().toString('hex');
+  tx = await nodeClient.getTX(_tx);
+
+  for(x=0;x<tx.outputs.length;x++) {
+    address=tx.outputs[x].address;
+    value=tx.outputs[x].value;
+    price=fs.readFileSync("/home/rasengan/dnslive/coingecko");
+    price=parseFloat(price);
+    if(tx.outputs[x].covenant.action=='REVEAL') {
+     name = Buffer.from(tx.inputs[0].coin.covenant.items[2],'hex').toString('ascii')
+     name = he.decode(punycode.toUnicode(name))
+     usd = parseFloat((value/1000000)*price).toFixed(2);
+     client.say('#handshake-live',address + " REVEALED a BID of "+value/1000000+" HNS ($"+usd+") for "+name);
+    }
+    else if(tx.outputs[x].covenant.action=='BID') {
+     name = Buffer.from(tx.outputs[x].covenant.items[2],'hex').toString('ascii')
+     name = he.decode(punycode.toUnicode(name))
+     usd = parseFloat((value/1000000)*price).toFixed(2);
+     client.say('#handshake-live',address + " BID "+value/1000000+" HNS ($"+usd+") for "+name);
+    }
+    else if(tx.outputs[x].covenant.action=='OPEN') {
+     name = Buffer.from(tx.outputs[x].covenant.items[2],'hex').toString('ascii')
+     name = he.decode(punycode.toUnicode(name))
+     client.say('#handshake-live',address+" OPENED "+name);
+    }
+  }
+});
+
